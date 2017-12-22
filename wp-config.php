7340d0bb9b121c3812731d42a3fdf7d611aef2f3
ubakarasamy <smuba4@gmail.com>
2017-12-22 16:49:28 +0530

    config change

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'uba_blog');
+define('DB_NAME', 'sketcnfw_wp_ubablog');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'sketcnfw_uba');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password');
+define('DB_PASSWORD', 'Admin123');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
